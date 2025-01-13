66d0355ef8ade9113d6c1f116208fe001b54a497
Ike1025 <143562131+Ike1025@users.noreply.github.com>
2025-01-12 19:26:42 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+int main() {
+    char string[] = "hello world";
+    printf("%s", string);
+    return 0;
+}
