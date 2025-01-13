bc2394763881b07ca847602d88db896c50d884ae
Ike1025 <143562131+Ike1025@users.noreply.github.com>
2025-01-13 18:48:23 -0500

    Basic falling

--- tetris.c
+++ tetris.c
@@ -2,17 +2,21 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 #define WIDTH 12
 #define HIGHT 20
 
 int gameover, score;
-int screen[WIDTH][HIGHT];
+int screen[HIGHT][WIDTH];
+int piece_x, piece_y;
 
 
 void setup() {
     gameover = 0;
     score = 0;
+    piece_x = -1;
+    piece_y = -1;
 }
 
 void draw() {
@@ -24,9 +28,9 @@ void draw() {
                 printf("|");
             } else {
                 if (screen[i][j] == 0) {
-                    printf("o");
-                } else {
                     printf(" ");
+                } else {
+                    printf("o");
                 }
             }           
         }
@@ -35,8 +39,27 @@ void draw() {
     
 }
 
+void logic() {
+    if (piece_x == -1) {
+        piece_x = 5;
+        piece_y = 0;
+        screen[piece_y][piece_x] = 1;
+        return;
+    }
+
+    if (piece_y < HIGHT-1) {
+        screen[piece_y][piece_x] = 0;
+        piece_y++;
+        screen[piece_y][piece_x] = 1;
+    } 
+}
+
 int main() {
     setup();
-    draw();
+    while (!gameover) {
+        draw();
+        logic();
+        Sleep(500);
+    }
     return 0;
 }
